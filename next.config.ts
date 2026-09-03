195c638db6138c24fd7364c5593d8264ea052959
sursade <113853145+sursade@users.noreply.github.com>
2026-09-03 14:07:17 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  // If using Next/Image:
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
